1ccddc8c2ed267852d98a770fa7dc37f70b49765
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-01-30 08:32:13 +0100

    add dependency injection required annotations

---
+++ src/main/java/com/webtracer/main/di/annotation/ExcludedWords.java
@@ -0,0 +1,24 @@
+package com.webtracer.main.di.annotation;
+
+import com.google.inject.BindingAnnotation;
+import jakarta.inject.Qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * {@code ExcludedWords} is a custom annotation used to mark the injection of
+ * a list of words that should be excluded from the word count process by the crawler.
+ * <p>
+ * This annotation is used with Google Guice to bind and inject the specific
+ * list of words that are to be ignored during the word counting operation,
+ * ensuring consistency across the application.
+ * <p>
+ * The annotation is retained at runtime to allow the dependency injection
+ * framework to use it during the injection process.
+ */
+@BindingAnnotation
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcludedWords {
+}
